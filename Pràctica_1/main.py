d738554029a9179d9c8a06295a0e230d2c5c9f7f
Mar-PVV <marvidal@institutperevives.cat>
2025-01-08 12:47:01 +0100

    Update main.py

--- Pràctica_1/main.py
+++ Pràctica_1/main.py
@@ -1,2 +1,8 @@
+# Importar funcions de funcions.py
+
 nombres = [3,5]
+resultat_mcm = calcul_mcm(nombres)
+resultat_MCD = calcul_MCD(nombres)
+
+# Fer els prints tal com indica l'enunciat (veure apartat Exemples d'execució - Sortida)
 
