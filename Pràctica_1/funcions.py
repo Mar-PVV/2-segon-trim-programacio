986a264c395a7d30b9ebefbc6d923a1c86e10b9c
Mar-PVV <marvidal@institutperevives.cat>
2025-01-09 13:26:46 +0100

    ajuda mcm

--- Pràctica_1/funcions.py
+++ Pràctica_1/funcions.py
@@ -1,6 +1,6 @@
 def prime(x, y):
     prime_list = []
-    for i in range(x, y):
+    for i in range(x, y+1):
         if i == 0 or i == 1:
             continue
         else:
@@ -12,6 +12,30 @@ def prime(x, y):
     return prime_list
 
 def factoritzacio_primers(num):
-    llista_primers = prime(1,num) # Us retorna per exemple: [2,3,5,7,11]
+    llista_primers = prime(1,num)
+    factors_primers = []
+
+    for num_primer in llista_primers:
+        if num != 1:
+            while num % num_primer == 0:
+                factors_primers.append(num_primer)
+                num = num / num_primer
+        else:
+            break
     
-    # Calcular llista de factors primers
+    return factors_primers
+
+def calcul_mcm(num1, num2):
+    llista1 = factoritzacio_primers(num1)
+    llista2 = factoritzacio_primers(num2)
+
+    mcm = 1
+
+    for factor in llista1:
+        if factor not in llista2:
+            mcm = mcm * factor
+        else:
+
+    
+
+    # El mcm seran tots els factors comuns i no comuns i en cas de comuns els de major exponent.
