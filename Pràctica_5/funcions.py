a334bcf6f631f5ac3d8ef8efee1de0b271d6d8c1
Mar-PVV <marvidal@institutperevives.cat>
2025-03-04 14:20:39 +0100

    llegir_registre corregit

--- Pràctica_5/funcions.py
+++ Pràctica_5/funcions.py
@@ -1,13 +1,15 @@
 import csv
 
 def llegir_registre():
-
+    registre = []
     with open('./Pràctica_5/registre.csv','r') as f:
         reader = csv.reader(f)
-        for row in reader:
-            print(row)
+        for index, row in enumerate(reader):
+            if index > 1:
+                registre.append([row[1],row[2]])
+    return registre
 
-llegir_registre()
+print(llegir_registre())
 
 def escriure_registre():
     pass
