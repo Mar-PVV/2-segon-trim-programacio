50e453a42950f8e305de0eeeaa0db3623c2b1dae
Mar-PVV <marvidal@institutperevives.cat>
2025-03-04 17:28:23 +0100

    final1

--- Pràctica_5/funcions.py
+++ Pràctica_5/funcions.py
@@ -1,18 +1,30 @@
 import csv
 
-def llegir_registre():
+def llegir_registre(nom_file):
     registre = []
-    with open('./Pràctica_5/registre.csv','r') as f:
+    with open('./Pràctica_5/registres/{nom_file}.csv','r') as f:
         reader = csv.reader(f)
         for index, row in enumerate(reader):
             if index > 1:
-                registre.append([row[1],row[2]])
+                registre.append([int(row[1]),int(row[2])])
     return registre
 
-print(llegir_registre())
-
-def escriure_registre(registre):
-    with open('./Pràctica_5/registre.csv','w') as f:
-        csv_writer = csv.writer(f, delimiter='w')
+def escriure_registre(nom_file, registre):
+    with open(f'./Pràctica_5/registres/{nom_file}.csv','w', newline='') as f:
+        csv_writer = csv.writer(f, delimiter=',')
         for index, row in enumerate(registre):
-            csv_writer.writerow([index,row[0],row[1]])
+            if index == 0:
+                fila = ['num_monedes','treure1','treure2']
+                csv_writer.writerow(fila)
+            else:
+                fila = [index,row[0],row[1]]
+                csv_writer.writerow(fila)
+
+def crear_registre(nom_file, num_monedes):
+    registre = []
+
+    # Crear taula de 0 de registre.
+    for i in range(num_monedes+1):
+        registre.append([0,0])
+    
+    escriure_registre(nom_file, registre)
